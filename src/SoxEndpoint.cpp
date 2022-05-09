174d01edd65755294da9010628f62e8c31b5f05a
= <rossgardiner24@gmail.com>
2022-05-09 20:46:18 +0100

    docs: SoxEndpoint

--- src/SoxEndpoint.cpp
+++ src/SoxEndpoint.cpp
@@ -1,17 +1,33 @@
 #include "SoxEndpoint.h"
 
 #include <string>
-
+/*!
+ * Open the SoX pipe
+ * @param srate sample rate for pipe
+ * @param nchan nr of channels in filtered audio
+ * @param globalopts
+ * @param fileopts
+ * @param filename filename to pass to sox
+ * @param effectopts options for SoX effects
+ * @return true if open sucessful, false otherwise
+ */
 bool SoxEndpoint::Open(int srate, int nchan, std::string globalopts, std::string fileopts, std::string filename, std::string effectopts) {
     std::string cmd("sox " + globalopts + "-t f64 -c " + std::to_string(nchan) + " -r " + std::to_string(srate) + " - " + fileopts + "'" + filename + "' " + effectopts);
     pipe = popen(cmd.c_str(), "w");
     return (pipe != nullptr);
 }
 
+/*!
+ * Adds the next sample to the output pipe
+ * @param s
+ */
 void SoxEndpoint::NextSample(double s) {
     fwrite(&s, sizeof(double), 1, pipe);
 }
 
+/*!
+ * Close the output pipe if it exists
+ */
 void SoxEndpoint::Close(void) {
     if(pipe != nullptr) {
         pclose(pipe);
--- src/SoxEndpoint.h
+++ src/SoxEndpoint.h
@@ -4,7 +4,9 @@
 #include "SampleCallback.h"
 #include <cstdio>
 #include <string>
-
+/*!
+ * A pipeline element which allows samples to be piped into the SoX program, written to file or otherwise.
+ */
 class SoxEndpoint : public SampleCallback {
 public:
     bool Open(int rate, int nchan, std::string globalopts, std::string fileopts, std::string filename, std::string effectopts);
