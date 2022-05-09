fcb9dde1f57c3750ba0cb5d28238a4ad88408829
= <rossgardiner24@gmail.com>
2022-05-09 17:12:22 +0100

    begins dnf integration

---
+++ src/FirDNF.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ross on 09/05/2022.
+//
+
+#include "FirDNF.h"
---
+++ src/FirDNF.h
@@ -0,0 +1,14 @@
+//
+// Created by ross on 09/05/2022.
+//
+
+#ifndef AUDICLEAN_FIRDNF_H
+#define AUDICLEAN_FIRDNF_H
+
+
+class FirDNF {
+
+};
+
+
+#endif //AUDICLEAN_FIRDNF_H
