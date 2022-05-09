8c0b504478b8fc18ff1196e8b99803d54f9609ea
= <rossgardiner24@gmail.com>
2022-05-09 20:39:28 +0100

    Docs: SampleCallback

--- src/SampleCallback.h
+++ src/SampleCallback.h
@@ -1,6 +1,9 @@
 #ifndef AUDICLEAN_SAMPLECALLBACK_H
 #define AUDICLEAN_SAMPLECALLBACK_H
 
+/*!
+ * A simple callback to pass audio samples through the filtering pipeline. Samples are treated as double-precision values in range [-1, 1]. Derived classes must implement the abstract function NextSample()
+ */
 class SampleCallback{
 public:
     virtual void NextSample(double next) = 0;
