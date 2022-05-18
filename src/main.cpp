60fc113286ebbf67d886caad6fcb44e6c0ee00dc
rossGardiner <rossgardiner24@gmail.com>
2022-05-18 13:16:02 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -76,7 +76,6 @@ int main(int argc, char ** argv) {
                 break;
             case 'D':
                 dnf = true;
-                lr = 1.0; //update to suit best dnf hyperparam
                 break;
             case 'q':
             case 'S':
